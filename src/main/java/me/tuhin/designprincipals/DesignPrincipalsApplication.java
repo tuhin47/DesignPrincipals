37fce1008bfc0dc36d0c6f415b4cefd344c9a8e5
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-01-12 19:55:07 +0600

    init

---
+++ src/main/java/me/tuhin/designprincipals/DesignPrincipalsApplication.java
@@ -0,0 +1,13 @@
+package me.tuhin.designprincipals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPrincipalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPrincipalsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/tuhin/designprincipals/DesignPrincipalsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tuhin.designprincipals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPrincipalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
