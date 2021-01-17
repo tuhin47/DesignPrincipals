864c2c325e83037055fcf9f3e49ae453910f8d5f
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2021-01-17 22:10:29 +0600

    template method pattern exercise test

--- src/test/java/me/tuhin/designprincipals/template/TemplateMethodPatternTest.java
+++ src/test/java/me/tuhin/designprincipals/template/TemplateMethodPatternTest.java
@@ -1,7 +1,10 @@
 package me.tuhin.designprincipals.template;
 
 import lombok.var;
+import me.tuhin.designprincipals.template.exercise.ChatWindow;
+import me.tuhin.designprincipals.template.exercise.ToolWindow;
 import me.tuhin.designprincipals.template.mosh.TransferMoneyTask;
+import me.tuhin.designprincipals.template.mosh.WithdrawMoneyTask;
 import org.junit.jupiter.api.Test;
 
 class TemplateMethodPatternTest {
@@ -9,7 +12,17 @@ class TemplateMethodPatternTest {
     @Test
     void templateMethodPatternMosh() {
         var task = new TransferMoneyTask();
+        var withdraw = new WithdrawMoneyTask();
+        withdraw.execute();
+        System.out.println();
         task.execute();
+        System.out.println();
+        //exercise
+        var toolWindow = new ToolWindow();
+        var chatWindow = new ChatWindow();
+        chatWindow.close();
+        System.out.println();
+        toolWindow.close();
     }
 
 }
